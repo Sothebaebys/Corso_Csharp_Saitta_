856b9af1822cd8177410c49cf1360e93c34f852e
Sothebaebys <176383400+Sothebaebys@users.noreply.github.com>
2026-02-19 15:00:26 +0100

    Metodi ed esercizi
    Updated:
    - aggiunto copie metodi di esempio
    - esercizi sui metodi

---
+++ 04_Gio_19_02_26/Esercizi/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class NomeClasse
+{
+   
+   // Primo esercizio
+   private static void StampaSaluto(string nome)
+   {
+      Console.WriteLine($"Ciao {nome}!!");
+   } 
+
+   //Secondo esercizio
+   public static void VerificaPari(int numero)
+   {
+      if ((numero % 2) == 0)
+      {
+         Console.WriteLine($"Il numero {numero} è pari");
+      }
+      else
+      {
+         Console.WriteLine($"Il numero {numero} è dispari");
+      }
+   }
+
+   //Terzo esercizio
+   private static int CalcoloPotenza(int baseNum, int esponente)
+   {
+      int potenza = baseNum;
+
+      if(baseNum != 0 && esponente >1)
+      {
+         for(int i = 1; i< esponente; i++)
+         //for(int i = (esponente-1); i> 0; i--)
+         {
+            potenza *= baseNum;
+         }
+         return potenza;
+      }
+      else
+      {
+         return 0;
+      }
+      
+   }
+
+
+   static void Main(string [] args)
+   {
+      StampaSaluto("Ciccio");
+      StampaSaluto("Fracchia");
+
+      VerificaPari(7);
+      VerificaPari(88);
+      VerificaPari(15);
+
+      Console.WriteLine($"2 elevato alla quinta dovrebbe corrispondere a : {CalcoloPotenza(2,5)}");
+      Console.WriteLine($"3 elevato alla terza dovrebbe corrispondere a :{CalcoloPotenza(3,3)}");
+
+
+   }
+
+}
---
+++ 04_Gio_19_02_26/Metodi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Metodi
+{
+   public static int Somma(int a, int b)
+   {
+      return a+b;
+   } 
+
+   
+   public static void Main(string [] args)
+   {
+      Console.WriteLine($"QUANTO FA QUINDICI PIU' DICIOTTTO!?!\n {Somma(15,18)}");
+   }
+}
